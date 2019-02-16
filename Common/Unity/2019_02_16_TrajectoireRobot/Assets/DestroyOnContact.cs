12c982ccf18d76effcbd4d02f61c192faeb71c0a
Eloi Stree <streeeloi@gmail.com>
2019-02-16 20:52:09 +0100

    Finial version 1 with Arthur

---
+++ Common/Unity/2019_02_16_TrajectoireRobot/Assets/DestroyOnContact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnContact : MonoBehaviour
+{
+    public LayerMask m_layerAllowToDestroy;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Touch", collision.gameObject);
+        if(Contains(m_layerAllowToDestroy, collision.gameObject.layer))
+            Destroy(this.gameObject);
+    }
+    public static bool Contains( LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+}
---
+++ Common/Unity/2019_02_16_TrajectoireRobot/Assets/SpawnGold.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGold : MonoBehaviour
+{
+    public GameObject goldPrefab;
+    public int count = 30;
+    public float range = 5;
+
+
+    void Start()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            CreateRandomGold();
+        }
+        
+    }
+
+    private void CreateRandomGold()
+    {
+        Vector3 position = this.transform.position;
+        Vector3 random = new Vector3(GetRandomFloat(), GetRandomFloat(), GetRandomFloat());
+        position += random;
+
+        Instantiate(goldPrefab, position, Quaternion.identity);
+    }
+
+    private float GetRandomFloat()
+    {
+        return UnityEngine.Random.Range(-range, range);
+    }
+}
